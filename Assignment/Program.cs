ccb90557444d45c748397680811fb186f3a8bc62
HusseinMahmoud98 <hussein.mahmoud.fouad98@gmail.com>
2025-08-24 19:17:04 +0300

    LINQ - Element Operators Q01

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,4 +1,5 @@
-﻿using static Assignment.ListGenerator;
+﻿using System.Xml.Linq;
+using static Assignment.ListGenerator;
 
 namespace Assignment
 {
@@ -6,6 +7,8 @@ namespace Assignment
     {
         static void Main(string[] args)
         {
+
+            #region LINQ - Restriction Operators
             // LINQ - Restriction Operators
             #region Q01
             //1. Find all products that are out of stock.
@@ -28,7 +31,7 @@ namespace Assignment
             //{
             //    Console.WriteLine(item);
             //} 
-            #endregion 
+            #endregion
             #endregion
 
             //2. Find all products that are in stock and cost more than 3.00 per unit.
@@ -41,22 +44,34 @@ namespace Assignment
             //}
 
             //Query Syntax
-            var result2 = from p in ProductList
-                          where p.UnitPrice > 3
-                          select p;
+            //var result2 = from p in ProductList
+            //              where p.UnitPrice > 3
+            //              select p;
 
-            foreach (var item in result2)
-            {
-                Console.WriteLine(item);
-            }
+            //foreach (var item in result2)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            #endregion
+
+            ////LINQ - Element Operators
+            #region Q01
+            ////1. Get first Product out of Stock 
+            ////Fluent Syntax
+            //var result = ProductList.Where(p=> p.UnitsInStock == 0).ElementAtOrDefault(0);  // will not throw exception
+            //Console.WriteLine(result);
+
+            ////or
+            //var result2 = ProductList.Where(p => p.UnitsInStock == 0).FirstOrDefault();  // will not throw exception
+            //Console.WriteLine(result2);
 
 
+            ////or
+            //var result3 = ProductList.FirstOrDefault(p => p.UnitsInStock == 0);  // will not throw exception
+            //Console.WriteLine(result3);
 
-
-
-
-
-
+            #endregion
 
         }
     }
