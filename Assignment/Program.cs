c510d2b9c47283e36ccd24aed6239455f9ccc4f2
HusseinMahmoud98 <hussein.mahmoud.fouad98@gmail.com>
2025-08-24 18:40:37 +0300

    LINQ - Restriction Operators Q01

---
+++ Assignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using static Assignment.ListGenerator;
+
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // LINQ - Restriction Operators
+            #region Q01
+            //1. Find all products that are out of stock.
+            #region Fluent Syntax
+            //Fluent Syntax
+            //var result = ProductList.Where(p => p.UnitsInStock == 0);
+
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //} 
+            #endregion
+
+            #region Query Syntax
+            //var result2 = from p in ProductList
+            //              where p.UnitsInStock == 0
+            //              select p;
+
+            //foreach (var item in result2)
+            //{
+            //    Console.WriteLine(item);
+            //} 
+            #endregion 
+            #endregion
+
+        }
+    }
+}
