a235562f38981bc0369ee8f55af9a1d1896b7065
HusseinMahmoud98 <hussein.mahmoud.fouad98@gmail.com>
2025-08-25 21:12:58 +0300

    Q01

---
+++ Task/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Return Numbers whose position is match their value int[]
+            int[] Arr = { 3, 4, 2, 0, 1, 5, 6 };
+
+            var result = Arr.Where((number, index) => number == index);
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item); //2, 5, 6
+            }
+        }
+    }
+}
