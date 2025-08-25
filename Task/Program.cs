1bc440d458a965bd571dff77c07d444fbd14487b
HusseinMahmoud98 <hussein.mahmoud.fouad98@gmail.com>
2025-08-25 21:20:18 +0300

    Q02

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,18 +1,38 @@
-﻿namespace Task
+﻿using static Task.ListGenerator;
+namespace Task
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //Return Numbers whose position is match their value int[]
-            int[] Arr = { 3, 4, 2, 0, 1, 5, 6 };
+            #region Q01
+            ////Return Numbers whose position is match their value int[]
+            //int[] Arr = { 3, 4, 2, 0, 1, 5, 6 };
 
-            var result = Arr.Where((number, index) => number == index);
+            //var result = Arr.Where((number, index) => number == index);
+
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item); //2, 5, 6 
+            #endregion
+
+            #region Q02
+            //Produce a sequence containing some properties of products, including unitprice
+            //which is renamed to price in the result
+            var result = ProductList.Select(
+                p => new
+                {
+                    p.ProductID,
+                    p.ProductName,
+                    Price = p.UnitPrice
+                });
 
             foreach (var item in result)
             {
-                Console.WriteLine(item); //2, 5, 6
-            }
+                Console.WriteLine(item);
+            } 
+            #endregion
         }
+        
     }
 }
